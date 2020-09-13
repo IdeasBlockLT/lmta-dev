8c9069bdd28540d731c8d7eda61db43366bb5ad3
Roberto <iorobertob@gmail.com>
2020-09-13 18:43:23 +0300

    added youtube error js

--- themes/bulma/parts/video.php
+++ themes/bulma/parts/video.php
@@ -1,7 +1,42 @@
 <div class="embed-responsive embed-responsive-16by9">
-    <iframe class="embed-responsive-item"
-            src="https://resourcespace.lmta.lt/filestore/1/6/2_85e8885fe9b2ec5/162_afefd9e4c5116d0.mp4"
+    <iframe class="embed-responsive-item" id="player" 
+            src="https://www.youtube.com/embed/live_stream?channel=UCws-DeDqDVzDRjYD6AXzjww&enablejsapi=1"
             allowfullscreen>
     </iframe>
     <!--                    <span class="video-thumbnail"></span>-->
 </div>
+
+<script src="https://www.youtube.com/player_api"></script>
+
+
+<script src="<?php echo get_stylesheet_directory_uri() . '/assets/js/youtube_functions.js'; ?>" type="module"></script>
+
+
+
+<script  type="text/javascript">
+
+    /**
+     * Need to wait until Youtube Player is ready!
+     */
+    // window.YT.ready(function() {
+    //     console.log("API");
+    //     player = new window.YT.Player("player", {
+    //     height: "390",
+    //     width: "640",
+    //     events: {
+    //       onReady: onPlayerReady,
+    //       onError: error_video
+    //     }
+    //   });
+    // });
+      
+    //   function onPlayerReady(event) {
+    //     event.target.playVideo();
+    //   }
+
+    // function error_video(event) {
+        
+    //     document.getElementById('player').src ='https://resourcespace.lmta.lt/filestore/1/6/2_85e8885fe9b2ec5/162_afefd9e4c5116d0.mp4';
+        
+    // }
+</script>
