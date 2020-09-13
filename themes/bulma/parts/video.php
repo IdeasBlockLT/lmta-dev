7b054b2e544e6c2c738766f61fd48b471f7d534c
Roberto <iorobertob@gmail.com>
2020-09-13 18:44:50 +0300

    added youtube error js

--- themes/bulma/parts/video.php
+++ themes/bulma/parts/video.php
@@ -11,32 +11,3 @@
 
 <script src="<?php echo get_stylesheet_directory_uri() . '/assets/js/youtube_functions.js'; ?>" type="module"></script>
 
-
-
-<script  type="text/javascript">
-
-    /**
-     * Need to wait until Youtube Player is ready!
-     */
-    // window.YT.ready(function() {
-    //     console.log("API");
-    //     player = new window.YT.Player("player", {
-    //     height: "390",
-    //     width: "640",
-    //     events: {
-    //       onReady: onPlayerReady,
-    //       onError: error_video
-    //     }
-    //   });
-    // });
-      
-    //   function onPlayerReady(event) {
-    //     event.target.playVideo();
-    //   }
-
-    // function error_video(event) {
-        
-    //     document.getElementById('player').src ='https://resourcespace.lmta.lt/filestore/1/6/2_85e8885fe9b2ec5/162_afefd9e4c5116d0.mp4';
-        
-    // }
-</script>
