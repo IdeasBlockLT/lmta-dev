42ff4dfe5806fb1d7d62b93a207e05ffd312da64
Roberto <iorobertob@gmail.com>
2020-09-18 16:47:45 +0300

    see if i CAN PRINT SOMETHING WHEN CREATING POST

--- themes/bulma/functions/resourcespace/LmtaRequest.php
+++ themes/bulma/functions/resourcespace/LmtaRequest.php
@@ -2,7 +2,7 @@
 
 function on_all_status_transitions( $new_status, $old_status, $post )       
 		{
-		   dd($old_status."_".$new_status);
+		   // dd($old_status."_".$new_status);
 		   if ( $new_status != $old_status ) {
 		    // A function to perform actions any time any post changes status.
 		    // dd("O L D   S T A T U S");
