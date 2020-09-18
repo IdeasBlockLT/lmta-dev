2f866e8c50659033599b466dd50b6b52118fca05
Roberto <iorobertob@gmail.com>
2020-09-18 18:50:58 +0300

    see if i CAN PRINT SOMETHING WHEN CREATING POST

--- themes/bulma/functions/resourcespace/LmtaRequest.php
+++ themes/bulma/functions/resourcespace/LmtaRequest.php
@@ -40,7 +40,7 @@ function on_all_status_transitions( $new_status, $old_status, $post )
 
 
 			    $image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0];
-			    $image_url = "https://stage.tv.ideas-block.com/wp-content/uploads/2020/09/FFTcircle_Offset_2-01.png";
+			    $image_url = "https%3A%2F%2Fstage.tv.ideas-block.com%2Fwp-content%2Fuploads%2F2020%2F09%2FFFTcircle_Offset_2-01.png";
 			    $new_id    = $resource->createResource($image_url);
 
 			    dd($new_id." : ".$image_url);
