1ae2b4de7d44e39234b27cbd5ae10b5f29e7ff55
Roberto <iorobertob@gmail.com>
2020-09-18 20:44:53 +0300

    trying to make a call to create resource when a posts status changes to publish, now not able to create resource

--- themes/bulma/functions/resourcespace/ResourceSpaceController.php
+++ themes/bulma/functions/resourcespace/ResourceSpaceController.php
@@ -87,12 +87,12 @@ class ResourceSpaceController
     public function createResource($image_url)
     {
 //        $query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&param7=" . urlencode(json_encode(array(1=>"Foo",8=>"Bar"))); # <--- The function to execute, and parameters
-        $this->$query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&archive=0&url=".$image_url;
+        $this->query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&archive=0&url=".$image_url;
         // $query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&archive=0";
         // dd($query);
         $response = $this->runBaby();
-        return $this->query;
-        // return $response;   
+        // return $this->query;
+        return $response;   
     }
 
     /**
