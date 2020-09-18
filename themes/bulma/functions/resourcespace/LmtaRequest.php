39cc098d7d72c823771a40dc9c6d17b93b932a08
Roberto <iorobertob@gmail.com>
2020-09-18 16:10:48 +0300

    see if i CAN PRINT SOMETHING WHEN CREATING POST

--- themes/bulma/functions/resourcespace/LmtaRequest.php
+++ themes/bulma/functions/resourcespace/LmtaRequest.php
@@ -1,5 +1,18 @@
 <?php
 
+function on_all_status_transitions( $new_status, $old_status, $post )       
+		{
+		   if ( $new_status != $old_status ) {
+		    // A function to perform actions any time any post changes status.
+		    dd("O L D   S T A T U S");
+		  }
+		  if ( $new_status != 'publish' ) {
+		    // A function to perform action when new post published.
+		    dd("P U B L I S H");
+		  }
+		}
+add_action(  'transition_post_status',  'on_all_status_transitions', 10, 3 );
+
 // Class stolen from diego
 class  LmtaRequest
 {
@@ -11,6 +24,18 @@ class  LmtaRequest
     {
         #for the variables for the api
         $this->title = "TITULO";
+
+        function on_all_status_transitions( $new_status, $old_status, $post )       
+		{
+		   if ( $new_status != $old_status ) {
+		    // A function to perform actions any time any post changes status.
+		  }
+		  if ( $new_status != 'publish' ) {
+		    // A function to perform action when new post published.
+		  }
+		}
+		add_action(  'transition_post_status',  'on_all_status_transitions', 10, 3 );
+
     }
 
 
