c525f20d203dd2a770ce2d35e4bf64988b1c8432
Roberto <iorobertob@gmail.com>
2020-09-19 13:50:34 +0300

    clean up, now we can create resources, but somehow only to diegos account, not to mediateka.lmta.lt

--- themes/bulma/functions/resourcespace/LmtaRequest.php
+++ themes/bulma/functions/resourcespace/LmtaRequest.php
@@ -28,30 +28,25 @@ function on_all_status_transitions( $new_status, $old_status, $post )
 		    if ($new_status === 'publish')
 		    {
 		    	// create resource in resourcespace
-		    	// dd("P U B L I S H");
-		    	// $resource = new ResourceSpaceController();
-			    // Get Id of this post and find out in RS if there is an image 
-			    // $ID = get_the_ID();
+			    // Get Id of this post
 			    $ID   = $post->ID;
-			    // $meta = get_post_meta($ID)["mediateka_title"][0];
+			    $title_field = get_post_meta($ID)["mediateka_title"][0]; // TODO: USE THIS TITLE TO GIVE METADATA TO THE NEW CREATED RESOURCE
 			    // $data = $resource->doSearch($meta);
 			    // dd($data[0]["file_extension"]);
-			    // $request = new LmtaRequest();
-			    // dd($request->title);
 
+			    $url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0];
+			    // $url = "https%3A%2F%2Fstage.tv.ideas-block.com%2Fwp-content%2Fuploads%2F2020%2F09%2FFFTcircle_Offset_2-01.png";
+				// $url = 'https://stage.tv.ideas-block.com/wp-content/uploads/2020/09/FFTcircle_Offset_2-01.png';// with diagonals
+			    $new_id    = $resource->createResource($url);
 
-			    // $url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0];
-			    $url = "https%3A%2F%2Fstage.tv.ideas-block.com%2Fwp-content%2Fuploads%2F2020%2F09%2FFFTcircle_Offset_2-01.png";
-				$url = 'https://stage.tv.ideas-block.com/wp-content/uploads/2020/09/FFTcircle_Offset_2-01.png';// with diagonals
-			    // $new_id    = $resource->createResource($url);
-
-			    // dd($new_id." : ".$url);
+			    dd($new_id." : ".$url);
 
 
 		    }
 		  }
 		  if ( $new_status != 'publish' ) {
 		    // A function to perform action when new post published.
+		    	$do = "nothing";
 		    
 		  }
 		}
--- themes/bulma/single.php
+++ themes/bulma/single.php
@@ -3,15 +3,11 @@
 
     // Get Id of this post and find out in RS if there is an image 
     $ID = get_the_ID();
-    $meta = get_post_meta($ID)["mediateka_title"][0];
-    // $data = $resource_controller->doSearch($meta);
-
-
-    $url = 'https://stage.tv.ideas-block.com/wp-content/uploads/2020/09/FFTcircle_Offset_2-01.png';// with diagonals
-    $data = $resource_controller->doSearch("trevio");
-    $data = $resource_controller->createResource($url);;
+    // $meta = get_post_meta($ID)["mediateka_title"][0];
+    // $url = wp_get_attachment_image_src( get_post_thumbnail_id( $ID ), 'single-post-thumbnail' )[0];
+    // $data = $resource_controller->createResource($url);;
     // dd($data[0]["file_extension"]);
-    dd("a ver : ".$data);
+    // dd("a ver : ".$data);
 
 
 //    if ($resource->hasVideo())
