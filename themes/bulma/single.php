a87c5b58159a2ca435ea03b90ad1f2d4b04f19eb
Roberto <iorobertob@gmail.com>
2020-09-19 13:55:24 +0300

    clean up, now we can create resources, but somehow only to diegos account, not to mediateka.lmta.lt

--- themes/bulma/single.php
+++ themes/bulma/single.php
@@ -1,11 +1,11 @@
 <?php
-    $resource_controller = new ResourceSpaceController();
+    $resource = new ResourceSpaceController();
 
     // Get Id of this post and find out in RS if there is an image 
     $ID = get_the_ID();
     // $meta = get_post_meta($ID)["mediateka_title"][0];
     // $url = wp_get_attachment_image_src( get_post_thumbnail_id( $ID ), 'single-post-thumbnail' )[0];
-    // $data = $resource_controller->createResource($url);;
+    // $data = $resource->createResource($url);;
     // dd($data[0]["file_extension"]);
     // dd("a ver : ".$data);
 
