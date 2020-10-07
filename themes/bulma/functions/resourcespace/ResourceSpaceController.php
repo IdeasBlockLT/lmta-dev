4bde83295598bb472988333023c88cf91923e5c7
Roberto <iorobertob@gmail.com>
2020-10-07 18:19:58 +0300

    in the controller file it should have been resourcespace_url and it was resourcespaceURL

--- themes/bulma/functions/resourcespace/ResourceSpaceController.php
+++ themes/bulma/functions/resourcespace/ResourceSpaceController.php
@@ -87,8 +87,8 @@ class ResourceSpaceController
     public function createResource($image_url)
     {
 //        $query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&param7=" . urlencode(json_encode(array(1=>"Foo",8=>"Bar"))); # <--- The function to execute, and parameters
-        // $this->query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&archive=0&url=".$image_url;
-        $this->query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&archive=0&url=". urlencode($image_url) . "&metadata=" . urlencode(json_encode(array(18=>"My captions",8=>"Česnakinis"))); # <--- The function to execute, and parameters
+        $this->query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&archive=0&url=".$image_url;
+        // $this->query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&archive=0&url=". urlencode($image_url) . "&metadata=" . urlencode(json_encode(array(18=>"My captions",8=>"Česnakinis"))); # <--- The function to execute, and parameters
         // $this->query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&archive=0";
         // $this->query="user=" . $this->apiUser . "&function=create_resource&param1=5&param2=0";
         // $this->query = $query = "user=" . $this->apiUser . "&function=do_search&param1='rast'";
