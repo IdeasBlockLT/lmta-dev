9ce7bb0a485ccb4c341780c164dd184de0fe23e2
Roberto <iorobertob@gmail.com>
2020-10-07 19:26:18 +0300

    add date to metadata

--- themes/bulma/functions/resourcespace/ResourceSpaceController.php
+++ themes/bulma/functions/resourcespace/ResourceSpaceController.php
@@ -88,7 +88,12 @@ class ResourceSpaceController
     {
 //        $query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&param7=" . urlencode(json_encode(array(1=>"Foo",8=>"Bar"))); # <--- The function to execute, and parameters
         // $this->query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&archive=0&url=".urlencode($image_url);
-        $this->query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&archive=0&url=". urlencode($image_url) . "&metadata=" . urlencode(json_encode(array(18=>"My captions",8=>"Česnakinis"))); # <--- The function to execute, and parameters
+        $this->query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&archive=0&url=". 
+        urlencode($image_url) . 
+        "&metadata=" . 
+        urlencode(json_encode(array(18=>"Custom captions blah blah",
+                                    8=>"Česnakinis",
+                                    12=>"2020-01-01 23:59"))); # <--- The function to execute, and parameters
         // $this->query="user=" . $this->apiUser . "&function=create_resource&resource_type=5&archive=0";
         // $this->query="user=" . $this->apiUser . "&function=create_resource&param1=5&param2=0";
         // $this->query = $query = "user=" . $this->apiUser . "&function=do_search&param1='rast'";
