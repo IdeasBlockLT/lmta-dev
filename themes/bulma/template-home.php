22f0e62dd1e6ae276b19bb7f83bf961e34596788
Roberto <iorobertob@gmail.com>
2020-10-12 01:21:47 +0300

    added conditions and ternary conditions to set variables of date, price, id etc, in the single

--- themes/bulma/template-home.php
+++ themes/bulma/template-home.php
@@ -31,9 +31,9 @@ $args = array(
             <div class="col-12 col-md-7 themed-grid-col mr-5">
                 <?php get_template_part('parts/video') ?>
                 <small class="d-block mt-4"><?php the_field('date'); ?></small>
-                <a style="text-decoration: none; 
-                                                color: black; " 
-                                                href="<? the_permalink(the_post()); ?>"><h3 class="hover-blue"><?= the_title(); ?></a></h3>
+                <a style="text-decoration: none;color: black; " 
+                   href="<? the_permalink(the_post()); ?>">
+                   <h3 class="hover-blue"><?= the_title(); ?></a></h3>
                 <p><?= the_excerpt(); ?></p>
             </div>
             <?php endif; ?>
