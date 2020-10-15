a1ac659fdbe005b761659aeda5c103dde03d3d13
Roberto <iorobertob@gmail.com>
2020-10-15 12:48:30 +0300

    added condition to find if we are live and change the info of the featured video

--- themes/bulma/template-home.php
+++ themes/bulma/template-home.php
@@ -32,7 +32,7 @@ $args = array(
 
                 <?php 
                     get_template_part('parts/video'); 
-                    setcookie("first_title", the_title(), time(), "/");
+                    setcookie("first_title", the_title());
                 ?>
 
 
