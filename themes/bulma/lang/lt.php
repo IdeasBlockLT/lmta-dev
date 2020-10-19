a0bb521dac263cbff6ab655f80cd1924cfc35146
Dimrak <diego.parte.plaza@gmail.com>
2020-10-19 23:57:20 +0300

    adding categories as string translations

--- themes/bulma/lang/lt.php
+++ themes/bulma/lang/lt.php
@@ -3,5 +3,14 @@
  * String translations, adding would show in wp-dashboard > languages > Strings Translations
  */
 
+//Loads all categories to show as strings translations
+$categories = get_categories();
+foreach ($categories as $category){
+    dump($category);
+    pll_register_string(strtolower($category->name), $category->name);
+}
+
 pll_register_string(strtolower('soon_events'), 'Artimiausi renginiai');
 pll_register_string(strtolower('read_more'), 'Skaityti daugiau');
+
+
