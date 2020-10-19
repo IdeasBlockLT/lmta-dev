4538f770a1c130406af98da03b19988511a78c4a
Dimrak <diego.parte.plaza@gmail.com>
2020-10-20 01:10:23 +0300

    No need, plugin work

--- themes/bulma/lang/lt.php
+++ themes/bulma/lang/lt.php
@@ -3,12 +3,6 @@
  * String translations, adding would show in wp-dashboard > languages > Strings Translations
  */
 
-//Loads all categories to show as strings translations
-$categories = get_categories();
-foreach ($categories as $category){
-    pll_register_string(strtolower($category->name), $category->name);
-}
-
 pll_register_string(strtolower('soon_events'), 'Artimiausi renginiai');
 pll_register_string(strtolower('read_more'), 'Skaityti daugiau');
 
