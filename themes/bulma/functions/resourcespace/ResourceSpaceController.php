9a327596f5a402fa2b0fa415b66f5bf2bdaccb24
Roberto <iorobertob@gmail.com>
2020-11-05 12:58:39 +0200

    for some reason the &s are not being read properly

--- themes/bulma/functions/resourcespace/ResourceSpaceController.php
+++ themes/bulma/functions/resourcespace/ResourceSpaceController.php
@@ -89,12 +89,11 @@ class ResourceSpaceController
 
         $this->query    ="user=" . $this->apiUser . "&function=create_resource".
                         "&param1=5".
-                        "&param2=0".
-                        "&param3=" .urlencode($image_url) . 
-                        "&param4=" .
-                        "&param5=" .
-                        "&param6=" .
-                        "&param7=" . 
+                        "&param2=0"."&param3=".urlencode($image_url). 
+                        "&param4=".
+                        "&param5=".
+                        "&param6=".
+                        "&param7=". 
                         urlencode(json_encode(array(18=>$date,
                                                     8 =>$title,
                                                     10=>$price,
