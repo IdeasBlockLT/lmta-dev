bb7a43c0630262aabee7c23474ac81b69689be7e
Roberto <iorobertob@gmail.com>
2020-11-06 00:34:14 +0200

    set the thumbnail in the single when there is nothing else to show

--- themes/bulma/single.php
+++ themes/bulma/single.php
@@ -63,7 +63,7 @@
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe class="embed-responsive-item" id="player" 
                             src="<?php echo($cover_url) ?>"
-                            allowfullscreen>
+                            allowfullscreen  scrolling='no' >
                     </iframe>
                 </div>
 
