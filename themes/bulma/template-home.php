3cbbf07882be2a08b28a2fc4cf4a3417bad52fff
Dimrak <diego.parte.plaza@gmail.com>
2020-11-07 18:02:34 +0200

    homepage style and fixes

--- themes/bulma/template-home.php
+++ themes/bulma/template-home.php
@@ -32,53 +32,59 @@ $args = [
         ?>
 
         <?php if ($query->have_posts()) : $query->the_post(); ?>
-
-
         <!--Will show the first from the query.-->
         <div class="row mb-0 mb-md-5">
-            <div class="col-12 col-md-7 themed-grid-col mr-5 first-post-div">
-
+            <div class="col-12 col-md-7 themed-grid-col mr-4 first-post-div">
                 <?php
                     get_template_part('parts/video'); 
                 ?>
-
                 <script>
                     document.cookie = "first_title=<?php the_title();?>";
                     document.cookie = "first_image=<?php the_post_thumbnail_url();?>";
                     document.cookie = "first_permalink=<?php the_permalink();?>";
                     console.log('<?php  the_field('streamDate');?>');
                 </script>
-
-
                 <small class="d-block mt-4 "><?php the_field('streamDate'); ?></small>
                 <a class="first-post-link" style="text-decoration: none;color: black; " 
                    href="<? the_permalink(); ?>">
                    <h3 class="hover-blue first-post"><?= the_title(); ?></h3></a>
                 <p class="first-post-excerpt"><?= the_excerpt(); ?></p>
+                <button class="mt-auto btn btn-light custom-more hover-blue__white">
+                    <a href="<?php echo get_the_permalink() ?>" class="">
+                        <?php echo strtoupper(pll_e('Skaityti daugiau')); ?>
+                    </a>
+                </button>
             </div>
         <?php endif; ?>
-
             <!--The rest of the videos-->
             <!--Need custom size of columns for wider columns-->
-            <div class="col-12 col-md-4 pl-4 themed-grid-col border-xl-left">
-                <div class="pb-4">
+            <div class="col-12 col-md-4 pl-4 themed-grid-col border-xl-left mt-4 mt-md-0 border-sm-top-invert">
+                <div class="ml-md-3">
                     <h4>
                         <strong><?php pll_e('Artimiausi renginiai'); ?></strong>
                     </h4>
                 </div>
                 <?php while ($query->have_posts()) :$query->the_post(); ?>
-                    <div class="themed-grid-col w-100 <?php echo (!get_next_post_link()) ? '' : 'border-bottom' ?>">
+                    <?php if (($query->current_post +1) == ($query->post_count)):?>
+                    <div class="themed-grid-col w-100 pb-4 pt-1 ml-md-3">
+                    <?php else: ?>
+                        <div class="themed-grid-col w-100 border-bottom pb-4 pt-1 ml-md-3">
+                    <?php endif; ?>
                         <br>
                         <small><?php the_field('streamDate'); ?></small>
-                        <a  style="text-decoration: none;color: black; " 
+                        <a style="text-decoration: none;color: black; "
                             href="<?php the_permalink(); ?>">
                         <h5 class="hover-blue"><?php the_title() ?></h5></a>
                         <p><?= the_excerpt(); ?></p>
+                        <button class="mt-auto btn btn-light custom-more hover-blue__white">
+                            <a href="<?php echo get_the_permalink() ?>" class="">
+                                <?php echo strtoupper(pll_e('Skaityti daugiau')); ?>
+                            </a>
+                        </button>
                     </div>
                 <?php endwhile; ?>
             </div>
         </div>
     </div>
-
-
 <?php get_footer(); ?>
+
