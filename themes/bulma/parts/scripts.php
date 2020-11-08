ffce668be689990b12700b1ba658d837836e87d9
Dimrak <diego.parte.plaza@gmail.com>
2020-11-08 12:12:18 +0200

    styles and cleanup

--- themes/bulma/parts/scripts.php
+++ themes/bulma/parts/scripts.php
@@ -7,7 +7,6 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-<script src="<?php echo get_stylesheet_directory_uri() . '/assets/js/ajax.js'; ?>" type="module"></script>
 <script src="<?php echo get_stylesheet_directory_uri() . '/assets/js/app.js'; ?>" type="module"></script>
 <script src="<?php echo get_stylesheet_directory_uri() . '/assets/js/toggle-calendar.js'; ?>" type="module"></script>
 <script src="https://cdn.jsdelivr.net/npm/javascript-obfuscator/dist/index.browser.js"></script>
--- themes/bulma/template-home.php
+++ themes/bulma/template-home.php
@@ -25,7 +25,7 @@ $args = [
 <?php get_template_part('parts/banner', null, ['size' => 'size1']); ?>
 <!--refs-->
     <div class="container w-90 mx-auto text-justify__home p-2">
-        <?php 
+        <?php
             $query = new WP_Query($args);
             $reverse = array_reverse($query ->posts);
             $query->posts = $reverse;
@@ -36,7 +36,7 @@ $args = [
         <div class="row mb-0 mb-md-5">
             <div class="col-12 col-md-7 themed-grid-col mr-4 first-post-div">
                 <?php
-                    get_template_part('parts/video'); 
+                    get_template_part('parts/video');
                 ?>
                 <script>
                     document.cookie = "first_title=<?php the_title();?>";
@@ -45,7 +45,7 @@ $args = [
                     console.log('<?php  the_field('streamDate');?>');
                 </script>
                 <small class="d-block mt-4 "><?php the_field('streamDate'); ?></small>
-                <a class="first-post-link" style="text-decoration: none;color: black; " 
+                <a class="first-post-link" style="text-decoration: none;color: black; "
                    href="<? the_permalink(); ?>">
                    <h3 class="hover-blue first-post"><?= the_title(); ?></h3></a>
                 <p class="first-post-excerpt"><?= the_excerpt(); ?></p>
@@ -55,21 +55,21 @@ $args = [
                     </a>
                 </button>
             </div>
-            
+
         <?php endif; ?>
             <!--The rest of the videos-->
             <!--Need custom size of columns for wider columns-->
             <div class="col-12 col-md-4 pl-4 themed-grid-col border-xl-left mt-4 mt-md-0 border-sm-top-invert">
-                <div class="ml-md-3">
+                <div class="ml-md-3 mb-0">
                     <h4>
                         <strong><?php pll_e('Artimiausi renginiai'); ?></strong>
                     </h4>
                 </div>
                 <?php while ($query->have_posts()) :$query->the_post(); ?>
                     <?php if (($query->current_post +1) == ($query->post_count)):?>
-                    <div class="themed-grid-col w-100 pb-4 pt-1 ml-md-3">
+                    <div class="themed-grid-col w-100 pb-2 pb-lg-4 ml-md-3">
                     <?php else: ?>
-                        <div class="themed-grid-col w-100 border-bottom pb-4 pt-1 ml-md-3">
+                        <div class="themed-grid-col w-100 border-bottom pb-2 pb-lg-4 ml-md-3">
                     <?php endif; ?>
                         <br>
                         <small><?php the_field('streamDate'); ?></small>
