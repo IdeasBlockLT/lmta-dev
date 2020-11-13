9acede38f17255a0f840d90d11f6ff7ecc9d2ffc
Roberto <iorobertob@gmail.com>
2020-11-14 00:20:27 +0200

    changes to youtube function js file and home template to hide date when youtube video is active

--- themes/bulma/template-home.php
+++ themes/bulma/template-home.php
@@ -46,8 +46,8 @@ $args = [
                 <small class="first-date d-block mt-4 "><?php the_field('streamDate'); ?></small>
                 <a class="first-post-link" style="text-decoration: none;color: black; "
                    href="<?php the_permalink(); ?>">
-                   <h3 class="hover-blue first-post"><?= the_title(); ?></h3></a>
-                <p class="first-post-excerpt"><?= the_excerpt(); ?></p>
+                   <h3 class="hover-blue first-post"><?= the_title();?></h3></a>
+                <p class="first-post-excerpt"><?= the_excerpt();?></p>
                 <button class="mt-auto btn btn-light custom-more hover-blue__white">
                     <a href="<?php the_permalink(); ?>" class="first-btn-a">
                         <?php echo strtoupper(pll_e('Skaityti daugiau')); ?>
