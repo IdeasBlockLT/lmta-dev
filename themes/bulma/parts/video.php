f7846ffc012f485c55b2b33c0cb70cbad1c2b1e1
ioroberto <iopaveqd@premium69.web-hosting.com>
2020-11-17 13:46:45 -0500

    added custom setting field for youtube default video

--- themes/bulma/parts/video.php
+++ themes/bulma/parts/video.php
@@ -1,10 +1,15 @@
-
+<?php 
+	$url = get_option("youtube_link_field","https://www.youtube.com/embed/live_stream?channel=UCws-DeDqDVzDRjYD6AXzjww&enablejsapi=1");
+	if($url == "" || $url == " " || $url == null){
+		$url = "https://www.youtube.com/embed/live_stream?channel=UCws-DeDqDVzDRjYD6AXzjww&enablejsapi=1";
+	}
+?>
 <div class="embed-responsive embed-responsive-16by9">
 <!-- <div class="wrap1">	 -->
     <iframe 
     		class="embed-responsive-item"
     		id="player" 
-            src="https://www.youtube.com/embed/live_stream?channel=UCws-DeDqDVzDRjYD6AXzjww&enablejsapi=1"
+            src="<?php echo($url);?>"
             allowfullscreen scrolling='no' >
     </iframe>
 </div>
