3bf407454ca49ec7043d36806a23dea59db6d848
ioroberto <iopaveqd@premium69.web-hosting.com>
2020-11-16 20:08:07 -0500

    fix video in home page, now it doesnt link to a particular post, they are shown 3 on the right. fix the duplicate creation of resources in resourcespace when the translation post is created, now it checks if only less than 1 posts contain the current post-s mediateka_title

--- themes/bulma/functions/resourcespace/LmtaRequest.php
+++ themes/bulma/functions/resourcespace/LmtaRequest.php
@@ -14,15 +14,13 @@ class  LmtaRequest
     }
 }
 
-
+/*
+ *  A function to perform actions any time any post changes status.
+ */
 function on_all_status_transitions( $new_status, $old_status, $post )       
 {
-   // dd($old_status."_".$new_status);
    if ( $new_status != $old_status ) {
-    	// A function to perform actions any time any post changes status.
-    	// dd("O L D   S T A T U S");
-    	$do = "nothing";
-   	
+    	
 	    if ($new_status === 'publish')
 	    {
 			// This function has to be called so that the metadata is readied
@@ -32,31 +30,35 @@ function on_all_status_transitions( $new_status, $old_status, $post )
 		    $resource = new ResourceSpaceController();
 		    $ID   			= $post->ID;
 		    $post_metadata 	= get_post_meta($ID);
-		    $title_field 	= $post_metadata["mediateka_title"][0]; // TODO: USE THIS TITLE TO GIVE METADATA TO THE NEW CREATED RESOURCE
+		    $title_field 	= $post_metadata["mediateka_title"][0];
 		    $date 			= $post_metadata["date"] [0];
-		    // $data = $resource->doSearch($meta);
-		    // dd($data[0]["file_extension"]);
 
 		    $url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0];
-		    // $url = "https%3A%2F%2Fstage.tv.ideas-block.com%2Fwp-content%2Fuploads%2F2020%2F09%2FFFTcircle_Offset_2-01.png";
-			// $url = 'https://stage.tv.ideas-block.com/wp-content/uploads/2020/09/FFTcircle_Offset_2-01.png';// with diagonals
 
-			// TODO: HERE CHECK FIRST IF RESOURCE ALREADY EXISTS WITH THIS NAME AND EDIT THAT ONE: !!!!!!!!!!!!!!!!!!!!!!!! 
-			// OR NOT? WHAT IF COVER IMAGE IS UPDATED, THAT WOULD OVERRIDE THE VIDEO IN RESOURCESPACE??????
-			// ONLY IF THIS IS A REAL POST, NOT A TRANSLATION (A REVISION)
-			// TODO: WHEN VISITING SINGLE, IF POST IS TRANSLATION, USER THE ID OF THE PARENT POST
-			if($post->post_type === 'post')
+			// HERE CHECK FIRST IF RESOURCE ALREADY EXISTS WITH THIS TODO:(if yes, find resourcein resourcespace, get its id and update metadata fields)
+			// This covers the case when this post is a translation. 
+			// USER WILL HAVE TO SAVE TO DRAFT FIRST, CONDITION BY DESIGN. 
+			$args = array(
+			   'meta_query' => array(
+				   array(
+					   'key' => 'mediateka_title',
+					   'value' => $title_field,
+					   'compare' => '=',
+				   )
+			   )
+			);
+			$query_posts = get_posts($args);
+// 			dd(count($query_posts).":".$title_field);
+			if( ($post->post_type === 'post') && (count($query_posts)<=1) )
 			{
 				$new_id    = $resource->createResourceV90($url, $title_field, $date, $price);
 			}
-
 		    // dd($new_id." : ".$url); 
 	    }
   }
   if ( $new_status != 'publish' ) {
-    // A function to perform action when new post published.
+    	// A function to perform action when new post published.
     	$do = "nothing";
-    
   }
 }
 
