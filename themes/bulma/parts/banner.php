e25f84a0dce1d33c0e190344bdbcad53b5edee82
Roberto <iorobertob@gmail.com>
2020-11-17 14:34:38 +0200

    change the color of the banner in blue pages

--- themes/bulma/parts/banner.php
+++ themes/bulma/parts/banner.php
@@ -3,8 +3,10 @@
 #Setting input-color
 if (!isset($args['background-color'])) {
     $background_color = 'white';
+    $text_color = "black";
 } else {
     $background_color = esc_html($args['background-color']);
+    $text_color = "white";
 }
 
 ?>
@@ -20,7 +22,7 @@ if (!isset($args['background-color'])) {
                     <img id="banner-logo" style="width: 6%" class="mx-auto d-inline-block align-self-center mr-3 img-thumbnail border-0 w-50 color-<?= $background_color ?>"
                          src="<?php bloginfo('stylesheet_directory') ?>/assets/images/logo_lmta.png" alt="" style="background-color: #5797fb00">
                 </a>
-                <strong class="custom-size-banner"><?php bloginfo('name') ?> <small id="tv" class="text-uppercase" style="display: table-caption"><?php bloginfo('description') ?></small></strong>
+                <strong class="custom-size-banner"><?php bloginfo('name') ?> <small id="tv" class="text-uppercase" style="color:"<?php echo($text_color); ?>"; display: table-caption"><?php bloginfo('description') ?></small></strong>
             </p>
         </div>
         <div class="col d-flex justify-content-end align-items-center">
