2c19632e8c834091c1dd5090a003937f33d98503
Roberto <iorobertob@gmail.com>
2020-11-20 19:43:40 +0200

    fix string typo

--- themes/bulma/parts/busimi-ivike.php
+++ themes/bulma/parts/busimi-ivike.php
@@ -2,7 +2,7 @@
     <div class="mb-4 custom-size">
         <h4 class="d-inline">
             <span value=">=" id="future-events"><?php pll_e('Būsimi renginiai');?></span>
-            <span value="<" id="past-events" class="text-muted">/ <?php pll_e('Įvykę renginiai');?></span>
+            <span value="<" id="past-events" class="text-muted">/ <?php pll_e('Įvikę renginiai');?></span>
         </h4>
     </div>
 </div>
