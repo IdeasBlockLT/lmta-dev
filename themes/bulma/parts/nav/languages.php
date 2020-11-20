aba1f45973dcd476627ebfcb51afdcc632083c50
Roberto <iorobertob@gmail.com>
2020-11-20 11:49:42 +0200

    change language menu

--- themes/bulma/parts/nav/languages.php
+++ themes/bulma/parts/nav/languages.php
@@ -26,7 +26,7 @@ if (is_page_template('template-mediateka.php')){
             <?php if ($current_lang_title !== $item->title): ?>
                 <li class="nav-item">
                     <a class="nav-link" href="<?= $item->url ?>">
-                        <i class="fas fa-long-arrow-alt-right mr-md-3 <?php echo $fontColor; ?>"></i>
+                        <i class="fas fa-long-arrow-alt-riight mr-md-3 <?php echo $fontColor; ?>"></i>
                         <strong class="extended-<?php echo $color; ?>"><?= ucfirst($item->title) ?> &nbsp; | </strong>
                     </a>
                 </li>
