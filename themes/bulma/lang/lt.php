009f502e759af8bbfd812b0970d9fbdb0dd2d16c
Roberto <iorobertob@gmail.com>
2020-11-20 21:42:51 +0200

    add string

--- themes/bulma/lang/lt.php
+++ themes/bulma/lang/lt.php
@@ -14,7 +14,7 @@ pll_register_string(strtolower('keywords'), 'Būsimi renginiai');
 pll_register_string(strtolower('keywords'), 'Įvikę renginiai');
 pll_register_string(strtolower('keywords'), 'Vaizdavimas');
 pll_register_string(strtolower('keywords'), 'Kūrėjai / atlikėjai');
-pll_register_string(strtolower('keywords'), 'Pograma');
+pll_register_string(strtolower('keywords'), 'Programa');
 
 /**
  * Declaring Constants to use later in template like this
