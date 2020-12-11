5c6ab3f33022be2e2b0c593571b46a8922c5a75e
Roberto <iorobertob@gmail.com>
2020-12-11 14:56:55 +0200

    add hours to elapsed time tooltip

--- themes/bulma/assets/js/custom-player.js
+++ themes/bulma/assets/js/custom-player.js
@@ -113,7 +113,16 @@ function updateSeekTooltip(event) {
   const skipTo = Math.round((event.offsetX / event.target.clientWidth) * parseInt(event.target.getAttribute('max'), 10));
   seek.setAttribute('data-seek', skipTo)
   const t = formatTime(skipTo);
-  seekTooltip.textContent = `${t.minutes}:${t.seconds}`;
+
+  if (t.hours != '00')
+  {
+    seekTooltip.textContent = `${t.hours}:${t.minutes}:${t.seconds}`;
+  }
+  else
+  {
+    seekTooltip.textContent = `${t.minutes}:${t.seconds}`;
+  }
+  
   const rect = video.getBoundingClientRect();
   seekTooltip.style.left = `${event.pageX - rect.left}px`;
 }
