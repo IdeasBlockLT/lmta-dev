2ab5a0ae6961c0207b8391ceef2053eee1fcc1cf
Roberto <iorobertob@gmail.com>
2020-12-11 14:34:16 +0200

    add typeof

--- themes/bulma/assets/js/custom-player.js
+++ themes/bulma/assets/js/custom-player.js
@@ -70,7 +70,8 @@ function initializeVideo() {
   seek.setAttribute('max', videoDuration);
   progressBar.setAttribute('max', videoDuration);
   const time = formatTime(videoDuration);
-  duration.innerText = `${time.minutes}:${time.seconds}`;
+  console.log(typeof time.hours);
+  duration.innerText = `${time.hours}:${time.minutes}:${time.seconds}`;
   duration.setAttribute('datetime', `${time.hours}h ${time.minutes}m ${time.seconds}s`)
 }
 
