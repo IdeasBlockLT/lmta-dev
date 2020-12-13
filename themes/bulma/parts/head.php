1437a8d057d45260fb839c60369131867c3489c4
Dimrak <diego.parte.plaza@gmail.com>
2020-12-13 21:04:27 +0200

    adding version to styles and current lang to head

--- themes/bulma/parts/head.php
+++ themes/bulma/parts/head.php
@@ -1,5 +1,8 @@
+<?php
+$current_lang = pll_current_language();
+?>
 <!doctype html>
-<html lang="en">
+<html lang="<?php echo $current_lang; ?>">
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -8,10 +11,9 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?><?php echo '?v=1.1' ?>">
     <title><?php bloginfo('name'); ?>></title>
     <!--Fontawesome-->
-<!--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">-->
     <script src="https://kit.fontawesome.com/1add32a990.js" crossorigin="anonymous"></script>
 
 </head>
