1183f843683ea4a1509a5d24d1a691d018cf1e76
Dimrak <diego.parte.plaza@gmail.com>
2020-12-13 21:07:05 +0200

    adding head info

--- themes/bulma/parts/head.php
+++ themes/bulma/parts/head.php
@@ -7,12 +7,12 @@ $current_lang = pll_current_language();
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?><?php echo '?v=1.1' ?>">
-    <title><?php bloginfo('name'); ?>></title>
+    <title><?php bloginfo('name'); ?></title>
     <!--Fontawesome-->
     <script src="https://kit.fontawesome.com/1add32a990.js" crossorigin="anonymous"></script>
 
